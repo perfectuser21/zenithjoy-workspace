bccdbf1143ae395ce0c8603278c5eac19698b6ac
Repo Lead <repo-lead@zenithjoy.local>
2026-05-21 14:49:32 +0800

    test(clips): route unit tests + controller stub

---
+++ apps/api/src/controllers/clips.controller.ts
@@ -0,0 +1,8 @@
+import { Request, Response, NextFunction } from 'express';
+export async function submitClip(_req: Request, res: Response, _next: NextFunction) { res.status(501).json({}); }
+export async function listClips(_req: Request, res: Response, _next: NextFunction) { res.status(501).json({}); }
+export async function getClipDetail(_req: Request, res: Response, _next: NextFunction) { res.status(501).json({}); }
+export async function retryClip(_req: Request, res: Response, _next: NextFunction) { res.status(501).json({}); }
+export async function handleCallback(_req: Request, res: Response, _next: NextFunction) { res.status(501).json({}); }
+export async function getUserSettings(_req: Request, res: Response, _next: NextFunction) { res.status(501).json({}); }
+export async function saveUserSettings(_req: Request, res: Response, _next: NextFunction) { res.status(501).json({}); }
